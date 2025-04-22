e120031f874b0086ff4fc14c78967b6c3a52319a
ajay <ajaykumar290904@gmail.com>
2025-04-22 11:07:48 +0530

    refactor: clean up Navbar component by removing unnecessary borders and improving spacing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,30 +9,30 @@ import logo from "../../public/logo.png";
 function Navbar() {
   return (
     <div className="flex justify-between px-6 py-2">
-      <div className="flex items-center border space-x-4 border-black ">
+      <div className="flex items-center  space-x-4 ">
         <AiOutlineMenu className="text-xl cursor-pointer" />
         <img src={logo} alt="" className="w-20 cursor-pointer" />
       </div>
 
-      <div className=" flex  w-[35%] items-center border border-black">
+      <div className=" flex  w-[35%] items-center">
         <div className="w-[100%] px-3 py-2 border rounded-l-full">
           <input type="text" placeholder="Search" className="outline-none " />
         </div>
-        <button className="px-4 py-2 border bg-gray-200 rounded-r-full ">
+        <button className=" px-4 py-2  bg-gray-200 rounded-r-full ">
           <CiSearch size={"24px"} />
         </button>
         <IoMdMic
           size={"42px"}
-          className=" ml-4 border rounded-full hover:bg-gray-300 duration-200 p-2 cursor-pointer"
+          className=" ml-4  rounded-full hover:bg-gray-300 duration-200 p-2 cursor-pointer"
         />
       </div>
-      <div className="flex items-center border border-black">
-        <div className="border  mr-7 rounded-full  ">
-          <RiVideoAddFill />
+      <div className="flex items-center  cursor-pointer">
+        <div className="   mr-7 rounded-full  ">
+          <RiVideoAddFill className="size-6" />
         </div>
 
         <div>
-          <IoIosNotifications />
+          <IoIosNotifications  className="size-8"/>
         </div>
 
         <div>
