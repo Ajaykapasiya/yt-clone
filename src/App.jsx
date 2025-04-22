e6a527332a3c2a155d71f411eec2690d97ba662e
ajay <ajaykumar290904@gmail.com>
2025-04-22 22:07:18 +0530

    feat: integrate useAuth hook to manage loading and data state in App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
+import { useAuth } from './context/AuthProvider'
 
 function App() {
 const {loading , data } = useAuth
+console.log(loading);
+console.log(data);
+
   return (
     <>
     <Navbar/>
