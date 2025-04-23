26dc6c55a2902e29b4a9d2c5c3b805987e0f08b3
ajay <ajaykumar290904@gmail.com>
2025-04-23 11:24:52 +0530

    feat: add routing and initial components for Home, Search, and PlayingVideo

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar";
-import { useAuth } from "./context/AuthProvider";
+import { Routes } from "react-router"
+import Home from "./components/Home";
+import Search from "./components/Search";
+import PlayingVideo from "./components/PlayingVideo";
+
+//import Sidebar from "./components/Sidebar";
+//import { useAuth } from "./context/AuthProvider";
 function App() {
-  const { loading, data } = useAuth();
-  console.log(loading);
-  console.log(data);
+ // const { loading, data } = useAuth();
+ // console.log(loading);
+ // console.log(data);
   return (
     <>
       <Navbar />
-      <Sidebar />
+      {/* <Sidebar /> */}
+      <Routes>
+        <Routes path="/" element={<Home/>}/>
+        <Routes path="/Search" element={<Search/>}/>
+        <Routes path="/PlayingVideo" element={<PlayingVideo/>}/>
+      </Routes>
     </>
   );
 }
