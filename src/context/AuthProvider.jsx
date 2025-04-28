31b211d9bc16e7c6407dd9e6f7b8c2915c797389
ajay <ajaykumar290904@gmail.com>
2025-04-29 00:21:56 +0530

    fix: correct data handling in AuthProvider and fix BASE_URL format in rapidApi.js

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -16,9 +16,9 @@ export default function AuthProvider ({children}){
 
     const fetchAlldata = (query) => {
         setLoading(true)
-        fetchData(`search/?q=${query}`).then(({contents}) => {
-            console.log(contents)
-            setData(contents) 
+        fetchData(`search/?q=${query}`).then((res) => {
+            //console.log(contents)
+            setData(res) 
             setLoading(false)
         })
     }
