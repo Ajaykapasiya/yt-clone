5760aeede981c169dfd4b24533cf4646f6b9bba5
ajay <ajaykumar290904@gmail.com>
2025-04-29 18:34:24 +0530

    feat: add Loading component and integrate loading state in App and Home components

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,17 @@ import { Routes, Route } from "react-router";
 import Home from "./components/Home";
 import Search from "./components/Search";
 import PlayingVideo from "./components/PlayingVideo";
-
+import Loading from "./loader/Loading";
 import Sidebar from "./components/Sidebar";
 import { useAuth } from "./context/AuthProvider";
 function App() {
-  const { loading, data } = useAuth();
-  console.log(loading);
-  console.log(data);
+  const {loading} = useAuth();
+  //const { loading, data } = useAuth();
+  //console.log(loading);
+  //console.log(data);
   return (
     <>
+    {loading && <Loading/>}
       <Navbar />
       {/* <Sidebar /> */}
       <Routes>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,9 +3,10 @@ import Sidebar from "./Sidebar";
 import Video from "./Video";
 import { useAuth } from "../context/AuthProvider";
 import ListItems from "./listItems";
+import Loading from "../loader/Loading";
 
 function Home() {
-  const { data } = useAuth();
+  const { data , loading } = useAuth();
 
   console.log(data);
 
@@ -14,9 +15,9 @@ function Home() {
       <Sidebar />
       <div className="h-[calc(100vh-6.625rem)] overflow-y-scroll overflow-x-hidden">
         <ListItems />
-      <div className="grid grid-cols-1 md:grid-cols-4 gap-5 p-5 ">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 p-5 ">
         
-        {Array.isArray(data) &&
+        {!loading &&
           data.map((item) => {
             if (item.type != "video") return false;
 
