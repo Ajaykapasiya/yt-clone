58808399974e6d4c51e6011e0ca152705eccad60
ajay <ajaykumar290904@gmail.com>
2025-04-29 19:12:40 +0530

    feat: implement video fetching logic in PlayingVideo component

--- src/components/PlayingVideo.jsx
+++ src/components/PlayingVideo.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import {fetchData} from '../utils/rapidApi'
+import { response } from 'express';
+
 
 function PlayingVideo() {
+  const [video , setVideo] = useState();
+  const [relatedVideo , setRelativeVideo] = useState();
+  const {id} = useParams()
+
+
+useEffect(() => {
+  fetchVideoDetails
+} , [id])
+
+const fetchVideoDetails = () => {
+  fetchData(`video/details/?id=${id}`).then((res) =>{
+    console.log(res)
+    setVideo(res)
+  })
+}
+
   return (
-    <div className='mt-32'>
-      Playing video
+    <div className=''>
+     
     </div>
   )
 }
