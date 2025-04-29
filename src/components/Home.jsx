ef5f68a2e279a1f77d1d146f229664e3c6338323
ajay <ajaykumar290904@gmail.com>
2025-04-29 14:25:34 +0530

    fix: adjust ListItems component placement in Home for improved layout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,14 +12,17 @@ function Home() {
   return (
     <div className="flex mt-20">
       <Sidebar />
+      <div className="h-[calc(100vh-6.625rem)] overflow-y-scroll overflow-x-hidden">
+        <ListItems />
       <div className="grid grid-cols-1 md:grid-cols-4 gap-5 p-5 ">
-        <ListItems/>
+        
         {Array.isArray(data) &&
           data.map((item) => {
             if (item.type != "video") return false;
 
             return <Video key={item.id} video={item?.video} />;
           })}
+      </div>
       </div>
     </div>
   );
