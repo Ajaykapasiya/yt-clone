68f8d4968b8788107e4dcfd5dc758c27d57024f6
ajay <ajaykumar290904@gmail.com>
2025-05-02 14:06:56 +0530

    feat: implement SuggestedVideo component; enhance video details display in PlayingVideo component

--- src/components/PlayingVideo.jsx
+++ src/components/PlayingVideo.jsx
@@ -1,23 +1,32 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { fetchData } from "../utils/rapidApi";
+import { fetchData } from "../utils/rapidapi";
 import ReactPlayer from "react-player";
-import { BsFillCheckCircleFill } from "react-icons/bs";
+import { AiOutlineLike } from "react-icons/ai";
 import { abbreviateNumber } from "js-abbreviation-number";
+import SuggestedVideo from "./SuggestedVideo";
+import { BsFillCheckCircleFill } from "react-icons/bs";
 
 function PlayingVideo() {
-  const [video, setVideo] = useState(null);
-  const [relatedVideo, setRelativeVideo] = useState([]);
+  const [video, setVideo] = useState();
+  const [realatedVideo, setRelativeVideo] = useState();
   const { id } = useParams();
 
   useEffect(() => {
     fetchVideoDetails();
+    fetchRelatedVideo();
   }, [id]);
 
   const fetchVideoDetails = () => {
     fetchData(`video/details/?id=${id}`).then((res) => {
-      console.log("Video Details:", res);
+      console.log(res);
       setVideo(res);
+    });
+  };
+  const fetchRelatedVideo = () => {
+    fetchData(`video/related-contents/?id=${id}`).then((res) => {
+      console.log(res);
+      setRelativeVideo(res);
     });
   };
 
@@ -35,28 +44,58 @@ function PlayingVideo() {
               playing={true}
             />
           </div>
-          <div>
-            {/* Video title */}
-            <span className="text-sm font-bold line-clamp-2">
-              {video.title}
-            </span>
-            <span className="flex items-center font-semibold mt-2 text-[12px] text-gray-500">
-              {video?.author?.title}
-              {video?.author?.badges[0]?.type === "VERIFIED_CHANNEL" && (
-                <BsFillCheckCircleFill className="text-gray-600 ml-1 text-[12px]" />
-              )}
-            </span>
-            <div>
-              <span className="flex text-gray-500 text-[12px]">
-                {`${abbreviateNumber(video?.stats?.views, 2)} views`}
-              </span>
-
-              <span className="flex text-[24px] leading-none font-bold relative top-[-10px] mx-1">
-                .
-              </span>
-              <span>{video?.publishedTimeText}</span>
+          <div className="font-bold text-sm md:text-xl mt-4 line-clamp-2">
+            {video?.title}
+          </div>
+          <div className="flex justify-between flex-col md:flex-row mt-4">
+            <div className="flex ">
+              <div className="flex items-start">
+                <div className="flex h-11 w-11 rounded-full overflow-hidden">
+                  <img
+                    className="h-full w-full object-cover"
+                    src={video?.author?.avatar[0]?.url}
+                  />
+                </div>
+              </div>
+              <div className="flex space-x-5">
+                <div className="flex flex-col ml-3">
+                  <div className="text-md font-semibold flex items-center">
+                    {video?.author?.title}
+                    {video?.author?.badges[0]?.type === "VERIFIED_CHANNEL" && (
+                      <BsFillCheckCircleFill className="text-white/[0.5] text-[12px] ml-1" />
+                    )}
+                  </div>
+                  <div className=" text-sm">
+                    {video?.author?.stats?.subscribersText}
+                  </div>
+                </div>
+                <span className="mt-1 text-center bg-red-500 px-3 pt-2 rounded-full text-white cursor-pointer hover:bg-red-700 duration-200 ">
+                  Subscribe
+                </span>
+              </div>
+            </div>
+            <div className="flex mt-4 md:mt-0">
+              <div className="flex items-center justify-center h-11 px-6 rounded-3xl bg-white/[0.15]">
+                <AiOutlineLike className="text-xl mr-2" />
+                {`${abbreviateNumber(video?.stats?.likes, 2)} Likes`}
+              </div>
+              <div className="flex items-center justify-center h-11 px-6 rounded-3xl bg-white/[0.15] ml-4">
+                {`${abbreviateNumber(video?.stats?.views, 2)} Views`}
+              </div>
             </div>
           </div>
+          <div className="p-4 bg-gray-100 rounded-xl mt-4 text-sm">
+            {video?.description}
+          </div>
+          <div className="flex gap-x-6 font-semibold rounded-xl mt-4 text-xl">
+            {video?.stats?.comments} <p>Comments</p>
+          </div>
+        </div>
+        <div className="flex flex-col px-4 py-6 h-[calc(100vh-4.625rem)] overflow-y-scroll overflow-x-hidden lg:w-[350px] xl:w-[400px]">
+          {realatedVideo?.contents?.map((item, index) => {
+            if (item?.type !== "video") return false;
+            return <SuggestedVideo key={index} video={item?.video} />;
+          })}
         </div>
       </div>
     </div>
