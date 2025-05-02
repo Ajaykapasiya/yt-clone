57de82e98a9805386c89fb05cf36e6cc305358d4
ajay <ajaykumar290904@gmail.com>
2025-05-02 14:34:38 +0530

    fix: update YouTube API key in .env; enhance SuggestedVideo component layout and styling

--- src/components/SuggestedVideo.jsx
+++ src/components/SuggestedVideo.jsx
@@ -1,62 +1,44 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Time from '../loader/Time'
-import { BsFillCheckCircleFill } from 'react-icons/bs'
+import React from "react";
+import { Link } from "react-router-dom";
+import Time from "../loader/Time";
+import { BsFillCheckCircleFill } from "react-icons/bs";
+import { abbreviateNumber } from "js-abbreviation-number";
 
-function SuggestedVideo({video}) {
+function SuggestedVideo({ video }) {
+  console.log(video);
   return (
     <div>
-      <Link to={`video/${video?.videoId}`}>
-        <div className="  flex flex-col">
-          {/* thumbnail */}
-          <div className=" relative h-24 md:h-20 xl:h-24 w-40 min-w-[168px] lg:w-32 lg:min-w-[ 128px] xl:w-[168px] md:rounded-xl hover:rounded-none duration-200 overflow-hidden">
+      <Link to={`/video/${video?.videoId}`}>
+        <div className="flex mb-3 ">
+          <div className="relative h-24 lg:h-20 xl:h-24 w-40 min-w-[168px] lg:w-32 lg:min-w-[128px] xl:w-40 xl:min-w-[168px] rounded-xl hover:rounded-none duration-200">
             <img
-              className="h-full w-full"
+              className="h-full w-full rounded-lg"
               src={video?.thumbnails[0]?.url}
-              alt=""
             />
-            {video?.lengthSeconds && <Time> time={video?.lengthSeconds}/</Time>}
+            {video?.lengthSeconds && <Time time={video?.lengthSeconds} />}
           </div>
-          {/* channel logo */}
-          <div className="flex mt-3 space-x-2 ">
-            <div className="flex items-start">
-              <div className="flex h-9 w-9 rounded-full overflow-hidden border">
-                <img
-                  className=" h-full  w-full rounded-full overflow-hidden"
-                  src={video?.author?.avatar[0].url}
-                  alt=""
-                />
-              </div>
-            </div>
-            <div>
-              {/* Video title */}
-              <span className="text-sm font-bold line-clamp-2">
-                {video.title}
+          <div className="flex flex-col ml-3 overflow-hidden">
+            <span className="text-sm lg:text-xs xl:text-sm font-bold line-clamp-2 ">
+              {video?.title}
+            </span>
+            <span className="text-[12px] lg:text-[10px] xl:text-[12px] font-semibold mt-2  flex items-center">
+              {video?.author?.title}
+              {video?.author?.badges[0]?.type === "VERIFIED_CHANNEL" && (
+                <BsFillCheckCircleFill className=" text-[12px] lg:text-[10px] xl:text-[12px] ml-1" />
+              )}
+            </span>
+            <div className="flex text-[12px] lg:text-[10px] xl:text-[12px] font-semibold  truncate overflow-hidden">
+              <span>{`${abbreviateNumber(video?.stats?.views, 2)} views`}</span>
+              <span className="flex text-[24px] leading-none font-bold  relative top-[-10px] mx-1">
+                .
               </span>
-              <span className="flex items-center font-semibold mt-2 text-[12px] text-gray-500">
-                {video?.author?.title}
-                {video?.author?.badges[0]?.type === "VERIFIED_CHANNEL" && (
-                  <BsFillCheckCircleFill className="text-gray-600 ml-1 text-[12px]" />
-                )}
-              </span>
-              <div>
-                <span className="flex text-gray-500 text-[12px]">
-                  {`${abbreviateNumber(video?.stats?.views, 2)} views`}
-                </span>
-
-                <span className="flex text-[24px] leading-none font-bold relative top-[-10px] mx-1">
-                  .
-                </span>
-                <span>
-                    {video?.publishedTimeText}
-                </span>
-              </div>
+              <span className="truncate">{video?.publishedTimeText}</span>
             </div>
           </div>
         </div>
       </Link>
     </div>
-  )
+  );
 }
 
-export default SuggestedVideo
+export default SuggestedVideo;
