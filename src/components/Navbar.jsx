20301acb24cc2dc6fea6f1e1199b931f04c76e42
ajay <ajaykumar290904@gmail.com>
2025-05-02 19:52:00 +0530

    feat: implement search functionality in Navbar component; enhance input handling and navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 import { CiSearch } from "react-icons/ci";
 import { IoMdMic } from "react-icons/io";
 import { RiVideoAddFill } from "react-icons/ri";
 import { IoIosNotifications } from "react-icons/io";
 import logo from "../../public/logo.png";
+import { useNavigate } from "react-router-dom";
 
 function Navbar() {
+
+  const [searchQuery , setSearchQuery] = useState("")
+  const navigate = useNavigate()
+  const searchQueryHandler = (event) => {
+    if((event?.key === "Enter" || event === "searchButton")&& searchQuery.length>0){
+      navigate(`/search/${searchQuery}`)
+      setSearchQuery("")
+    } 
+  }
+
   return (
     <div className="flex justify-between fixed top-0 w-[100%] bg-white px-6 py-2">
       <div className="flex items-center  space-x-4 ">
@@ -16,10 +27,18 @@ function Navbar() {
 
       <div className=" flex  w-[35%] items-center">
         <div className="w-[100%] px-4 py-2 border border-gray-200 rounded-l-full">
-          <input type="text" placeholder="Search" className="outline-none " />
+          <input type="text" placeholder="Search" className="outline-none " 
+          onChange={(e) => setSearchQuery(e.target.value)}
+          onKeyUp={searchQueryHandler}
+          value={searchQuery}
+          />
         </div>
-        <button className=" px-4 py-2  bg-gray-200 rounded-r-full ">
-          <CiSearch size={"26px"} />
+        <button className=" px-4 py-2  bg-gray-200 rounded-r-full "
+          
+          onClick={() => searchQueryHandler("searchButton")}
+
+        >
+          <CiSearch  size={"26px"} />
         </button>
         <IoMdMic
           size={"42px"}
