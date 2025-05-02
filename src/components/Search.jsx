2a9b527941dcbaa897f7731f79b2bbc6fc04dc5e
ajay <ajaykumar290904@gmail.com>
2025-05-02 18:12:53 +0530

    feat: implement search functionality in Search component; enhance layout and styling

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useAuth } from "../context/AuthProvider";
+import { fetchData } from "../utils/rapidapi";
+import Sidebar from "./Sidebar";
 
 function Search() {
+  const [search, setResult] = useState();
+  const { searchQuery } = useParams();
+
+  useEffect(() => {
+    fetchSearchResults();
+  }, [searchQuery]);
+
+  const fetchSearchResults = () => {
+    fetchData(`search/?q=${searchQuery}`).then(({ contents }) => {
+      console.log(contents);
+      setResult(contents);
+    });
+  };
+
   return (
-    <div className="mt-18">
-      search
-      
+    <div className="">
+      <div className="mt-24 flex flex-row h-[calc(100%-56px)]">
+        <Sidebar />
+        <div className="grow h-[calc(100vh-6.625rem)] overflow-y-scroll overflow-x-hidden">
+          <div className=" grid grid-cols-1 gap-2 p-2">
+            {resultf?.map((item, index) => {
+              if (item?.type !== "video") return false;
+              return <SearchCard key={index} video={item?.video} />;
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
