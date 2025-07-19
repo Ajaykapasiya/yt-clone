4bebbfb16c3ef53eeb2ba596ff66149d8b70b45a
ajay <ajaykumar290904@gmail.com>
2025-07-19 18:50:39 +0530

    Fix rapidApi import case sensitivity in all files

--- src/components/PlayingVideo.jsx
+++ src/components/PlayingVideo.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { fetchData } from "../utils/rapidapi";
+import { fetchData } from "../utils/rapidApi";
 import ReactPlayer from "react-player";
 import { AiOutlineLike } from "react-icons/ai";
 import { abbreviateNumber } from "js-abbreviation-number";
